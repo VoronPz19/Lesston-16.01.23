55bac36c851aaec2494f4d562c3b09f4a11eb0e1
VoronPz19 <eshik3.11@gmail.com>
2023-01-23 20:54:01 +0700

    added the basic structure of the project

---
+++ Lession17-18(16.12.22)(NameVariations)/js/name-variations.js
@@ -0,0 +1,20 @@
+/* name-variations.js */
+
+export default class NameVariations {
+  /** @param {string} name */
+  constructor(name) {
+		this.name = name;
+  }
+
+  getNumberOfChars() {
+		return this.name.length
+  }
+
+  getLower() {
+		return this.name.toLocaleLowerCase()
+  }
+
+  getUpper() {
+		return this.name.toLocaleUpperCase()
+  }
+}
