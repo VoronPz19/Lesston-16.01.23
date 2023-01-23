6df953fbb7d37f3491c2624d116d01c6ffd6b1c0
VoronPz19 <eshik3.11@gmail.com>
2023-01-23 20:59:57 +0700

    added the structure of the project writer

---
+++ Lesston17-18(16.01.23)(Writer)/src/js/index.js
@@ -0,0 +1,18 @@
+/* index.js */
+import User from "./user.js";
+
+class Writer extends User {
+    publishArticle() {
+        return "Article published"
+    }
+}
+
+
+
+// do not modify
+const user = new User('Sam', 'Smith', 30);
+console.log(user); // User {firstName: 'Sam', lastName: 'Smith', age: 30}
+
+const writer = new Writer('Tom', 'Roth', 20);
+console.log(writer); // Writer {firstName: 'Tom', lastName: 'Roth', age: 20}
+console.log(writer.publishArticle()); // Article published
