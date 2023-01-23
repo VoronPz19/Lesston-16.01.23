5c4e2f497dbe0bd0c4a9785e5fb0cbff559884b6
VoronPz19 <eshik3.11@gmail.com>
2023-01-23 20:59:47 +0700

    added the structure of the project recipe

---
+++ Lesston17-18(16.01.23)(recipe)/src/js/index.js
@@ -0,0 +1,10 @@
+/* index.js */
+import Recipe from "./recipe.js";
+
+// class usage
+const pasta = new Recipe("Pasta", 700);
+console.log(pasta);
+
+
+const salad = new Recipe("Salad", 350);
+console.log(salad);
